51b5e36ea558947ef820579b4509f6689d23cb6d
Yamuna <yamunaoli87@gmail.com>
2026-04-12 17:28:20 +0530

    fix package test controller

--- src/main/java/com/homeeco/controller/HomeController.java
+++ src/main/java/com/homeeco/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.homeeco.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
-
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "HomeEco Backend is Running 🚀";
+        return "index"; // loads index.html
     }
 }
