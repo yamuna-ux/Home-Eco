a5236d7bca687869d4fbee04c3b0f4c46a18708e
Yamuna <yamunaoli87@gmail.com>
2026-04-14 10:00:19 +0530

    Final CORS fix

--- src/main/java/com/homeeco/config/CorsConfig.java
+++ src/main/java/com/homeeco/config/CorsConfig.java
@@ -10,10 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500"
-            )
+           .allowedOrigins("*")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(false);
